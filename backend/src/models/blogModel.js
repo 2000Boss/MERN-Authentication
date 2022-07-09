203350c7c296183d6c731760dcedba28fef06349
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2022-07-09 19:57:49 +0600

    models are created

---
+++ backend/src/models/blogModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: [200, "Maximum length for the title is 200"],
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  datePublished: {
+    type: Date,
+    default: Date.now,
+  },
+  photoLink: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: [10, "Minimum length for the description is 10"],
+  },
+  body: {
+    type: String,
+    required: true,
+    minlength: [100, "Minimum length for the description is 100"],
+  },
+});
+
+const Blogs = mongoose.model("Blogs", BlogSchema, "Blog");
+
+module.exports = Blogs;
