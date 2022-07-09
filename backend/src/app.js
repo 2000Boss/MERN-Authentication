f5e935554639fddf81b5b23c8304b3169b830b54
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2022-07-09 19:15:37 +0600

    created app and server files of backend and error is handled

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.enable("trust proxy");
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+module.exports = app;
